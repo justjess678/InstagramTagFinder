0ae30ce13cba8a7d6eb780ca9da2b7f8736b13d6
Jessica Chambers <jess.chambers@gmail.com>
2019-03-21 22:37:31 +0100

    Added initial script

---
+++ instagaram_tags.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 20:59:00 2019
+
+@author: Jessica Chambers
+"""
+from urllib.request import urlopen
+import re
+import operator
+# The tag you want to look up
+tag = "crochet"
+# Add re listed words here
+redlist = ["artesanato","hechoamano", "ganchillo", "knitting", "feitoamano", \
+           "fauxlocs", "tapetedecroche", "knit", "croche", "moda"]
+# Number of tags desired
+num_of_tags = 60
+
+link = "https://www.instagram.com/explore/tags/"+tag+"/?hl=en"
+
+for i in range(0,30):
+    html = urlopen(link).read()
+    html = str(html)
+    
+    m = re.findall(r'{"text":(.*?)}', html)
+    tag_class = {}
+    for txt in m:
+        tmp = re.findall(r'#(.*?) ',txt)
+        for t in tmp:
+            if "\\" in t or "#" in t or t == tag or len(t) > 40 or t in redlist:
+                tmp.remove(t)
+            else:
+                if t in tag_class:
+                    tag_class[t] = tag_class.get(t) + 1
+                else:
+                    tag_class[t] = 1
+
+tag_class_sorted = dict(sorted(tag_class.items(), key=operator.itemgetter(1), reverse=True)[:num_of_tags])
+print(tag_class_sorted)
