82fcfca263abe924a507aa619f09afc1c52ac78e
Jessica Chambers <jess.chambers@gmail.com>
2019-03-21 22:52:05 +0100

    Added comments

--- instagaram_tags.py
+++ instagaram_tags.py
@@ -10,6 +10,10 @@ import re
 import operator
 # The tag you want to look up
 tag = ["crochet","fashion"]
+# Clean the tags (in case of user input)
+for i in range(0,len(tag)):
+    tag[i] = tag[i].toLower()
+    tag[i] = tag[i].replace(" ","")
 # Add re listed words here
 redlist = ["artesanato","hechoamano", "ganchillo", "knitting", "feitoamano", \
            "fauxlocs", "tapetedecroche", "knit", "croche", "moda"]
@@ -17,24 +21,28 @@ redlist = ["artesanato","hechoamano", "ganchillo", "knitting", "feitoamano", \
 num_of_tags = 30
 link = []
 for t in tag:
- link.append("https://www.instagram.com/explore/tags/"+t+"/?hl=en")
+    link.append("https://www.instagram.com/explore/tags/"+t+"/?hl=en")
 
 for i in range(0,10):
     for l in link:
         html = urlopen(l).read()
         html = str(html)
+        # Parse the page and isolate photo captions
         m = re.findall(r'{"text":(.*?)}', html)
         tag_class = {}
         for txt in m:
+            # Isolate the hashtags from the photo captions
             tmp = re.findall(r'#(.*?) ',txt)
             for t in tmp:
+                # Remove unwanted tags
                 if "\\" in t or "#" in t or t in tag or len(t) > 40 or t in redlist:
                     tmp.remove(t)
                 else:
+                    # Count the occurances of the hashtags
                     if t in tag_class:
                         tag_class[t] = tag_class.get(t) + 1
                     else:
                         tag_class[t] = 1
-
+# Sort the top amount of tags
 tag_class_sorted = dict(sorted(tag_class.items(), key=operator.itemgetter(1), reverse=True)[:num_of_tags])
 print(tag_class_sorted)
