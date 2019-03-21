575946e7d562e8d8639bb89803bd472d56d9c856
Jessica Chambers <jess.chambers@gmail.com>
2019-03-21 22:46:13 +0100

    Added multiple tag handling

--- instagaram_tags.py
+++ instagaram_tags.py
@@ -9,31 +9,32 @@ from urllib.request import urlopen
 import re
 import operator
 # The tag you want to look up
-tag = "crochet"
+tag = ["crochet","fashion"]
 # Add re listed words here
 redlist = ["artesanato","hechoamano", "ganchillo", "knitting", "feitoamano", \
            "fauxlocs", "tapetedecroche", "knit", "croche", "moda"]
 # Number of tags desired
-num_of_tags = 60
+num_of_tags = 30
+link = []
+for t in tag:
+ link.append("https://www.instagram.com/explore/tags/"+t+"/?hl=en")
 
-link = "https://www.instagram.com/explore/tags/"+tag+"/?hl=en"
-
-for i in range(0,30):
-    html = urlopen(link).read()
-    html = str(html)
-    
-    m = re.findall(r'{"text":(.*?)}', html)
-    tag_class = {}
-    for txt in m:
-        tmp = re.findall(r'#(.*?) ',txt)
-        for t in tmp:
-            if "\\" in t or "#" in t or t == tag or len(t) > 40 or t in redlist:
-                tmp.remove(t)
-            else:
-                if t in tag_class:
-                    tag_class[t] = tag_class.get(t) + 1
+for i in range(0,10):
+    for l in link:
+        html = urlopen(l).read()
+        html = str(html)
+        m = re.findall(r'{"text":(.*?)}', html)
+        tag_class = {}
+        for txt in m:
+            tmp = re.findall(r'#(.*?) ',txt)
+            for t in tmp:
+                if "\\" in t or "#" in t or t in tag or len(t) > 40 or t in redlist:
+                    tmp.remove(t)
                 else:
-                    tag_class[t] = 1
+                    if t in tag_class:
+                        tag_class[t] = tag_class.get(t) + 1
+                    else:
+                        tag_class[t] = 1
 
 tag_class_sorted = dict(sorted(tag_class.items(), key=operator.itemgetter(1), reverse=True)[:num_of_tags])
 print(tag_class_sorted)
