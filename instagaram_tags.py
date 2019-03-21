e8acfe2fa86325a16036dd8f4ed4bd7246622c11
Jessica Chambers <jess.chambers@gmail.com>
2019-03-21 23:25:25 +0100

    Separated into functions

--- instagaram_tags.py
+++ instagaram_tags.py
@@ -8,6 +8,9 @@ Created on Thu Mar 21 20:59:00 2019
 from urllib.request import urlopen, HTTPError
 import re
 import operator
+
+tag_class = {}
+
 # The tag you want to look up
 tag = ["crochet","fashion"]
 # Clean the tags (in case of user input)
@@ -24,29 +27,29 @@ for t in tag:
     for i in range(1,16):
         link.append("https://www.instagram.com/explore/tags/"+t+"/?hl=en&?page="+str(i))
 
+def get_caption(html):
+    return re.findall(r'{"text":(.*?)}', html)
 
-for l in link:
-    try:
-        html = urlopen(l).read()
-        html = str(html)
-        # Parse the page and isolate photo captions
-        m = re.findall(r'{"text":(.*?)}', html)
-        tag_class = {}
-        for txt in m:
-            # Isolate the hashtags from the photo captions
-            tmp = re.findall(r'#(.*?) ',txt)
-            for t in tmp:
-                # Remove unwanted tags
-                if "\\" in t or "#" in t or t in tag or len(t) > 40 or t in redlist:
-                    tmp.remove(t)
+def get_hashtag_dict(tag, redlist, caption):
+    for txt in caption:
+        # Isolate the hashtags from the photo captions
+        tmp = re.findall(r'#(.*?) ',txt)
+        for t in tmp:
+            # Remove unwanted tags
+            if "\\" in t or "#" in t or t in tag or len(t) > 40 or t in redlist:
+                tmp.remove(t)
+            else:
+                # Count the occurances of the hashtags
+                if t in tag_class:
+                    tag_class[t] = tag_class.get(t) + 1
                 else:
-                    # Count the occurances of the hashtags
-                    if t in tag_class:
-                        tag_class[t] = tag_class.get(t) + 1
-                    else:
-                        tag_class[t] = 1
-    except HTTPError:
-        print('Could not download page')
+                    tag_class[t] = 1
+    #print(tag_class)
+# MAIN   
+for l in link:
+    html = urlopen(l).read()
+    html = str(html)
+    get_hashtag_dict(tag, redlist, get_caption(html))
 # Sort the top amount of tags
 tag_class_sorted = dict(sorted(tag_class.items(), key=operator.itemgetter(1), reverse=True)[:num_of_tags])
 print(tag_class_sorted)
