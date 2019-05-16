a10e38e7766241dba11b886de3e29423f90e1e99
Jessica Chambers <jess.chambers@gmail.com>
2019-05-16 19:37:18 +0200

    Added email handling from account

--- email_tags.py
+++ email_tags.py
@@ -7,24 +7,33 @@ Created on Tue May 14 17:23:59 2019
 """
 
 
-import smtplib
+import smtplib, re
 
 def sendEmail(destaddr, msg):
-    sender = ""
-    sender_email = ""
-    message = "From: From Person <from@fromdomain.com>"
-    message += "To: To Person <"+destaddr+">"
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", destaddr):
+        raise Exception('invalid_email')
+    pw = "#tags4all"
+    sender = "Tag Finder"
+    sender_email = "tagfinderforinsta@gmail.com"
+    receiver = destaddr.split('@')[0]
+    message = "From: "+sender+" <"+sender_email+">"
+    message += "To: "+receiver+" <"+destaddr+">"
     message += """MIME-Version: 1.0
     Content-type: text/html
     Subject: Your Instagram tags are ready!
 
-    <h1>Your hashtags have finsihed brewing!</h1>
+    <h1>Your hashtags have finished brewing!</h1>
     <p>
     """
-    
+    message += """</p><br>
+    <h3>If you like this app, <a href="paypal.me/JessChambers27">
+    consider making a donation!</a></h3>
+    """
     try:
-       smtpObj = smtplib.SMTP('localhost')
-       smtpObj.sendmail(sender, receivers, message)         
+       server = smtplib.SMTP('smtp.gmail.com', 587)
+       server.ehlo()
+       server.starttls()
+       server.sendmail(sender, destadr, message)         
        print("Successfully sent email")
     except SMTPException:
        print("Error: unable to send email")
