4522e42125b52e4f3bc2c8907d63c591a7a39dbb
Jessica Chambers <jess.chambers@gmail.com>
2019-07-11 16:43:42 +0200

    Retrieves *all* followers followers

--- instagram_login.py
+++ instagram_login.py
@@ -25,6 +25,8 @@ TOKEN
 import operator, time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 username = "scrapeme2019"
 password = "ilovescraping"
@@ -37,9 +39,11 @@ LOGGED = False
 
 def load(length=5):
     for i in range(0,length):
-        print(length-i,'... ')
+        print('. ', end =" ")
         time.sleep(1)
-
+    print("")
+    
+    
 def login():
     
     # prepare the option for the chrome driver
@@ -76,51 +80,120 @@ def get_users_following(user):
     load(3)
     try:
         following_button = driver.find_element_by_xpath('//a[@href="/'+user+'/following/"]')
+        total_following = int(following_button.text[:-10])
         following_button.click()
         load(3)
-        all_following = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate _0imsa ']")
-        out = [follow.text for follow in all_following]
+        # Load all followers (scroll down div item)
+        loaded_following = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate _0imsa ']")
+        loaded_till_now = len(loaded_following)
+        
+        while(loaded_till_now<total_following):
+            print("following users loaded till now: " + str(loaded_till_now) + " / " + str(total_following))
+            loaded_following[loaded_till_now-1].location_once_scrolled_into_view
+            driver.find_element_by_tag_name('body').send_keys(Keys.END) # triggers AJAX request to load more users. observed that loading 10 users at a time.
+            load(1) 
+            loaded_following = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate _0imsa ']")
+            loaded_till_now = len(loaded_following)
+        print("following users loaded till now: " + str(total_following) + " / " + str(total_following))
+        out = [follow.text for follow in loaded_following]
     except NoSuchElementException:
         out =[]
     return out
 
 
-def get_users_followers(user):
+def get_users_followers(user, second=False):
     if not LOGGED:
         login()
     driver.get(url+user+'/')
     load(3)
     try:
+        #click the followers button
         followers_button = driver.find_element_by_xpath('//a[@href="/'+user+'/followers/"]')
+        total_following = int(followers_button.text[:-10])
         followers_button.click()
         load(3)
-        all_followers = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate _0imsa ']")
+        # Load all followers (scroll down div item)
+        loaded_following = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate _0imsa ']")
+        loaded_till_now = len(loaded_following)
+        
+        while(loaded_till_now<total_following):
+            print("following users loaded till now: " + str(loaded_till_now) + " / " + str(total_following))
+            loaded_following[loaded_till_now-1].location_once_scrolled_into_view
+            driver.find_element_by_tag_name('body').send_keys(Keys.END) # triggers AJAX request to load more users. observed that loading 10 users at a time.
+            load(1) 
+            loaded_following = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate _0imsa ']")
+            loaded_till_now = len(loaded_following)
+        print("following users loaded till now: " + str(total_following) + " / " + str(total_following))
         second_following = []
-        followers = [follow.text for follow in all_followers]
+        followers = [follow.text for follow in loaded_following]
+        
         for follower in followers:
             print(follower)
-            second_following.extend(get_users_following(follower))
-            load(2)
-            print("Next follower:")
+            if second:
+                second_following.extend(get_users_following(follower))
+                load(2)
+            else:
+                second_following.append(follower)
+        return second_following
     except NoSuchElementException:
-        second_following =[]
-    return second_following
+        return []
+
+
+def get_users_followed_hashtags(users):
+    if not LOGGED:
+        login()
+    followers = []
+    for user in users:
+        driver.get(url+user+'/')
+        load(3)
+        try:
+            followers_button = driver.find_element_by_xpath('//a[@href="/'+user+'/followers/"]')
+            followers_button.click()
+            load(3)
+            #click on hashtags
+            hastags_button = driver.find_element_by_xpath('//button[@class="_0mzm- sqdOP yWX7d    _8A5w5  "]')
+            hastags_button.click()
+            load(2)
+            loaded_following = driver.find_elements_by_xpath("//a[@class='hI7cq']")
+            followers.extend([follow.text for follow in loaded_following])
+        except NoSuchElementException:
+            pass
+    return followers
+        
 
 def get_top_users(list_users, num_of_users = 20):
     user_class = {}
     for u in list_users:
-        if u in user_class:
-            user_class[u] = user_class.get(u) + 1
-        else:
-            user_class[u] = 1
+        if list_users.count(u) > 1:
+            user_class[u] = list_users.count(u)
     user_class_sorted = dict(sorted(user_class.items(), key=operator.itemgetter(1), reverse=True))
     return user_class_sorted
+
+
+def get_top_followed_tags(list_tags, num_of_tags = 20):
+    tag_class = {}
+    for t in list_tags:
+        if list_tags.count(t) > 1:
+            tag_class[t] = list_tags.count(t)
+    tag_class_sorted = dict(sorted(tag_class.items(), key=operator.itemgetter(1), reverse=True))
+    return tag_class_sorted
             
 LOGGED = login()
 if LOGGED: 
-    followers = get_users_followers('jesscomix')
-    top = get_top_users(followers)
-    print(top)
+    followers = get_users_followers('transatlanticcrochet')
+    top_followers = get_top_users(followers)
+    tags = get_users_followed_hashtags(followers)
+    top_followers_tags = get_top_followed_tags(tags)
+    print("-------------------TOP FOLLOWED------------")
+    f= open("top_followed.txt","w+")
+    f.write(top_followers)
+    f.close()
+    print(top_followers)
+    print("-------------------TOP TAGS------------")
+    f= open("top_followed_tags.txt","w+")
+    f.write(top_followers_tags)
+    f.close()
+    print(top_followers_tags)
     print("all done")
 
 driver.quit();
